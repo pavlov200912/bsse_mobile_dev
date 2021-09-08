c6aa40852a37a7125b96a3efb05080761ac4d062
Ivan Pavlov <pavlov200912@gmail.com>
2021-09-08 14:29:47 +0300

    delete tests (dependencies and dirs)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,13 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            Snackbar.make(
-                findViewById(R.id.myLayout),
-                "Alexandrina lox",
-                Snackbar.LENGTH_SHORT
-            ).show()
-        }
     }
 }
