465f18a72e8af980e97d3acaa6abcd69bfc09fbe
Ivan Pavlov <pavlov200912@gmail.com>
2021-12-29 13:51:14 +0300

    email confirmation now with api

--- app/src/main/java/com/example/myapplication/data/net/MockApi.kt
+++ app/src/main/java/com/example/myapplication/data/net/MockApi.kt
@@ -11,55 +11,79 @@ import com.example.myapplication.entity.User
 import com.haroldadmin.cnradapter.NetworkResponse
 
 class MockApi : Api {
+    private val users = mutableListOf<User>()
+
+    private val token = AuthTokens(
+        accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiYWRtaW4iLCJpYXQiOjE0MjI3Nzk2MzgsImV4cCI6MTY0MDg3MTc3MX0.gzSraSYS8EXBxLN_oWnFSRgCzcmJmMjLiuyu5CSpyHI",
+        refreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiYWRtaW4iLCJpYXQiOjE0MjI3Nzk2MzgsImV4cCI6MTY0MDg3MTc3MX0.gzSraSYS8EXBxLN_oWnFSRgCzcmJmMjLiuyu5CSpyHI",
+        accessTokenExpiration = 1640871771000,
+        refreshTokenExpiration = 1640871771000,
+    )
+
     override suspend fun getUsers(): NetworkResponse<List<User>, Unit> {
         return NetworkResponse.Success(
-            listOf(User(
-                avatarUrl = "https://memepedia.ru/wp-content/uploads/2020/10/big-floppa-meme.png",
-                userName = "Shlepa",
-                groupName = "MCS",
-                id = 1,
-                firstName = "Shlepa",
-                lastName = "Pelmenev"
-            )),
+            listOf(
+                User(
+                    avatarUrl = "https://memepedia.ru/wp-content/uploads/2020/10/big-floppa-meme.png",
+                    userName = "Shlepa",
+                    groupName = "MCS",
+                    id = 1,
+                    firstName = "Shlepa",
+                    lastName = "Pelmenev"
+                )
+            ),
             code = 200
         )
     }
 
     override suspend fun signInWithEmail(request: SignInWithEmailRequest): NetworkResponse<AuthTokens, SignInWithEmailErrorResponse> {
         return NetworkResponse.Success(
-            AuthTokens(
-                accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiYWRtaW4iLCJpYXQiOjE0MjI3Nzk2MzgsImV4cCI6MTY0MDg3MTc3MX0.gzSraSYS8EXBxLN_oWnFSRgCzcmJmMjLiuyu5CSpyHI",
-                refreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiYWRtaW4iLCJpYXQiOjE0MjI3Nzk2MzgsImV4cCI6MTY0MDg3MTc3MX0.gzSraSYS8EXBxLN_oWnFSRgCzcmJmMjLiuyu5CSpyHI",
-                accessTokenExpiration = 1640871771000,
-                refreshTokenExpiration = 1640871771000,
-            ),
+            token,
             code = 200
         )
     }
-    override suspend fun refreshAuthTokens(request: RefreshAuthTokensRequest): NetworkResponse<AuthTokens, RefreshAuthTokensErrorResponse> {
-        TODO("Not yet implemented")
-    }
 
     override suspend fun sendRegistrationVerificationCode(email: String): NetworkResponse<Unit, SendRegistrationVerificationCodeErrorResponse> {
-        TODO("Not yet implemented")
+        return NetworkResponse.Success(Unit, code = 200)
     }
 
     override suspend fun verifyRegistrationCode(
         code: String,
         email: String
     ): NetworkResponse<VerificationTokenResponse, VerifyRegistrationCodeErrorResponse> {
+        if (code == "123456") {
+            return NetworkResponse.Success(VerificationTokenResponse(code), code = 200)
+        }
+        return NetworkResponse.ServerError(
+            VerifyRegistrationCodeErrorResponse(listOf(Error("wrong code!"))),
+            code = 400
+        )
+    }
+
+    override suspend fun refreshAuthTokens(request: RefreshAuthTokensRequest): NetworkResponse<AuthTokens, RefreshAuthTokensErrorResponse> {
         TODO("Not yet implemented")
     }
 
+
     override suspend fun createProfile(request: CreateProfileRequest): NetworkResponse<AuthTokens, CreateProfileErrorResponse> {
-        TODO("Not yet implemented")
+        users.add(
+            User(
+                1,
+                "Ivan",
+                "Olejnik",
+                "olen",
+                "https://pixnio.com/free-images/2021/03/30/2021-03-30-11-06-07-1800x1200.jpg",
+                "mkn",
+            )
+        )
+        return NetworkResponse.Success(token, code = 200)
     }
 
     override suspend fun getPosts(): NetworkResponse<List<Post>, Unit> {
         return NetworkResponse.Success(
             listOf(
                 Post(
-                    id=1,
+                    id = 1,
                     linkUrl = "https://developer.android.com/training/dependency-injection/hilt-android",
                     imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Cawood_sword_-_hilt_-_YORYM_2007_3086.JPG/1200px-Cawood_sword_-_hilt_-_YORYM_2007_3086.JPG",
                     title = "Android hilt",
@@ -68,7 +92,7 @@ class MockApi : Api {
                     updatedAt = ""
                 ),
                 Post(
-                    id=2,
+                    id = 2,
                     linkUrl = null,
                     imageUrl = "https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg",
                     title = "Cat",
@@ -77,7 +101,7 @@ class MockApi : Api {
                     updatedAt = ""
                 ),
                 Post(
-                    id=3,
+                    id = 3,
                     title = "Hello world",
                     text = "Some text with post",
                     createdAt = "",
--- app/src/main/java/com/example/myapplication/ui/emailconfirmation/EmailConfirmationFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/emailconfirmation/EmailConfirmationFragment.kt
@@ -1,21 +1,36 @@
 package com.example.myapplication.ui.emailconfirmation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentEmailConfirmationBinding
 import com.example.myapplication.ui.base.BaseFragment
+import com.example.myapplication.ui.signup.SignUpViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import dev.chrisbanes.insetter.applyInsetter
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
+@AndroidEntryPoint
 class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmation) {
-    val viewModel: EmailConfirmationViewModel by viewModels()
     private val viewBinding by viewBinding(FragmentEmailConfirmationBinding::bind)
+
+    private val viewModel: EmailConfirmationViewModel by viewModels()
+    private val signUpViewModel: SignUpViewModel by hiltNavGraphViewModels(R.id.signUpFragment)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val signUpForm = signUpViewModel.signUpData ?: error("Sign up data not set up")
+
         viewBinding.backButton.applyInsetter {
             type(statusBars = true) { margin() }
         }
@@ -24,6 +39,68 @@ class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmat
         }
         viewBinding.backButton.setOnClickListener {
             findNavController().popBackStack()
+        }
+        viewBinding.confirmButton.setOnClickListener {
+            viewModel.signUpWithCode(
+                signUpForm.firstname,
+                signUpForm.lastname,
+                signUpForm.nickname,
+                signUpForm.email,
+                signUpForm.password,
+                viewBinding.verificationCodeEditText.getCode()
+            )
+        }
+
+        // open email app
+        viewBinding.openEmailButton.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_MAIN
+                ).apply {
+                    addCategory(Intent.CATEGORY_APP_EMAIL)
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+            )
+        }
+
+        subscribeToFormFields()
+        subscribeToEvents()
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.sendVerificationCode(signUpForm.email)
+        }
+    }
+
+    private fun subscribeToFormFields() {
+        viewBinding.confirmButton.isEnabled = false
+        viewBinding.verificationCodeEditText.onVerificationCodeFilledChangeListener = {
+            viewBinding.confirmButton.isEnabled = it
+        }
+    }
+
+    private fun subscribeToEvents() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.emailConfirmationActionStateFlow().collect { event ->
+                    when (event) {
+                        EmailConfirmationViewModel.EmailConfirmationActionState.Loading -> {
+                            // nothing
+                        }
+                        EmailConfirmationViewModel.EmailConfirmationActionState.Pending -> {
+                            // nothing
+                        }
+                        is EmailConfirmationViewModel.EmailConfirmationActionState.NetworkError ->
+                            Timber.d(event.e.toString())
+                        is EmailConfirmationViewModel.EmailConfirmationActionState.ServerError ->
+                            Timber.d(event.e.toString())
+                        is EmailConfirmationViewModel.EmailConfirmationActionState.EmailVerificationError ->
+                            Timber.d(event.e.toString())
+                        is EmailConfirmationViewModel.EmailConfirmationActionState.UnknownError ->
+                            Timber.d(event.e.toString())
+
+                    }
+                }
+            }
         }
     }
 }
