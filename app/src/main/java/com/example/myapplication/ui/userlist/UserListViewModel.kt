3568b1f2627785a7f73cccaf55c912b15182716b
Ivan Pavlov <pavlov200912@gmail.com>
2022-01-02 18:47:29 +0300

    Handle corner case

--- app/src/main/java/com/example/myapplication/ui/userlist/UserListViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/userlist/UserListViewModel.kt
@@ -1,14 +1,10 @@
 package com.example.myapplication.ui.userlist
 
 import androidx.lifecycle.viewModelScope
-import com.example.myapplication.BuildConfig
-import com.example.myapplication.data.net.Api
-import com.example.myapplication.data.net.MockApi
 import com.example.myapplication.entity.User
 import com.example.myapplication.interactor.UsersInteractor
 import com.example.myapplication.ui.base.BaseViewModel
 import com.haroldadmin.cnradapter.NetworkResponse
-import com.squareup.moshi.Moshi
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -16,9 +12,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -36,7 +29,7 @@ class UserListViewModel @Inject constructor(
     }
 
 
-    private fun loadUsers() {
+    fun loadUsers() {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
                 Timber.d("load users()")
@@ -45,10 +38,13 @@ class UserListViewModel @Inject constructor(
                 _viewState.emit(ViewState.Loading)
                 when (val response = usersInteractor.loadUsers()) {
                     is NetworkResponse.Success -> {
-                        _viewState.emit(ViewState.Data(response.body))
+                        if (response.body.isEmpty())
+                            _viewState.emit(ViewState.EmptyList)
+                        else
+                            _viewState.emit(ViewState.Data(response.body))
                     }
                     else -> {
-
+                        _viewState.emit(ViewState.Error)
                     }
                 }
             }
@@ -59,6 +55,8 @@ class UserListViewModel @Inject constructor(
     sealed class ViewState {
         object Loading : ViewState()
         data class Data(val userList: List<User>) : ViewState()
+        object EmptyList : ViewState()
+        object Error : ViewState()
     }
 
 }
